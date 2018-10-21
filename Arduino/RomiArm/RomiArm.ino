bc7ff386307c218ebb26eb826ba3b1899c3aae35
Peter Klemperer <peter@peterklemperer.com>
2018-10-20 22:43:25 -0400

    added a sample arm control arduino application.

--- Arduino/RomiArm/RomiArm.ino
+++ Arduino/RomiArm/RomiArm.ino
@@ -12,65 +12,72 @@
   Shoulder PWM: 1000 (raised, 60 deg), 1900 (lowered, 0 deg)
   Wrist PWM: 1200 (down, 0 deg), 1900 (up, 110 deg)
 
+  NEXT TASKS:
+  * Add printing for sampling potentiometer tap inputs
+  * Add methods for converting potentiometer taps to positions
+
   http://github.com/ftPeter/RomiArm
 */
 
 #include <Servo.h>
 
+
+// These pins correspond to my 328PB configuration
+// SERVO PINS
+#define GRIPPER_SERVO_PIN (10)
+#define WRIST_SERVO_PIN (11)
+#define SHOULDER_SERVO_PIN (3)
+// SERVO POTENTIOMETER TAP PINS
+#define GRIPPER_POT_PIN (A4)
+#define WRIST_POT_PIN (A5)
+#define SHOULDER_POT_PIN (A6)
+
 Servo gripper_servo;
 Servo wrist_servo;
 Servo shoulder_servo;
 
+int gripper_pot;
+int wrist_pot;
+int shoulder_pot;
+
 void setup() {
-  // put your setup code here, to run once:
-  gripper_servo.attach(10);
-  wrist_servo.attach(11);
-  shoulder_servo.attach(3);
-}
+  gripper_servo.attach(GRIPPER_SERVO_PIN);
+  wrist_servo.attach(WRIST_SERVO_PIN);
+  shoulder_servo.attach(SHOULDER_SERVO_PIN);
 
-int gripper_opening_to_pwm( int opening_mm ) {
-  opening_mm = constrain( opening_mm, 0, 32 );
-  int gripper_pwm = map( opening_mm, 0, 32, 500, 2400 );
-  return gripper_pwm;
-}
-
-int wrist_angle_to_pwm( int wrist_deg ) {
-  wrist_deg = constrain( wrist_deg, 0, 110 );
-  int wrist_pwm = map( wrist_deg, 0, 110, 1200, 1900 );
-  return wrist_pwm;
-}
-
-int shoulder_angle_to_pwm( int shoulder_deg ) {
-  shoulder_deg = constrain( shoulder_deg, 0, 60 );
-  int shoulder_pwm = map( shoulder_deg, 0, 60, 1900, 1000 );
-  return shoulder_pwm;
+  pinMode(GRIPPER_POT_PIN, INPUT);
+  pinMode(WRIST_POT_PIN, INPUT);
+  pinMode(SHOULDER_POT_PIN, INPUT);
 }
 
 void loop() {
+
+  // move the gripper up and open
   for (int i = 0; i < 100; i++) {
-    int gripper_opening = map( i, 0, 99, 0, 32 );
-    int gripper_pwm = gripper_opening_to_pwm( gripper_opening );
+    int gripper_opening_mm = map( i, 0, 99, 0, 32 );
+    int gripper_pwm = gripper_opening_to_pwm( gripper_opening_mm );
 
-    int wrist_angle = map( i, 0, 99, 0, 110 );
-    int wrist_pwm = wrist_angle_to_pwm( wrist_angle );
+    int wrist_angle_deg = map( i, 0, 99, 0, 110 );
+    int wrist_pwm = wrist_angle_to_pwm( wrist_angle_deg );
 
-    int shoulder_angle = map( i, 0, 99, 0, 60 );
-    int shoulder_pwm = shoulder_angle_to_pwm( shoulder_angle );
+    int shoulder_angle_deg = map( i, 0, 99, 0, 60 );
+    int shoulder_pwm = shoulder_angle_to_pwm( shoulder_angle_deg );
 
     gripper_servo.write(gripper_pwm);
     wrist_servo.write(wrist_pwm);
     shoulder_servo.write(shoulder_pwm);
   }
 
+  // move the gripper down and closed
   for (int i = 99; i >= 0; i--) {
-    int gripper_opening = map( i, 0, 99, 0, 32 );
-    int gripper_pwm = gripper_opening_to_pwm( gripper_opening );
+    int gripper_opening_mm = map( i, 0, 99, 0, 32 );
+    int gripper_pwm = gripper_opening_to_pwm( gripper_opening_mm );
 
-    int wrist_angle = map( i, 0, 99, 0, 110 );
-    int wrist_pwm = wrist_angle_to_pwm( wrist_angle );
+    int wrist_angle_deg = map( i, 0, 99, 0, 110 );
+    int wrist_pwm = wrist_angle_to_pwm( wrist_angle_deg );
 
-    int shoulder_angle = map( i, 0, 99, 0, 60 );
-    int shoulder_pwm = shoulder_angle_to_pwm( shoulder_angle );
+    int shoulder_angle_deg = map( i, 0, 99, 0, 60 );
+    int shoulder_pwm = shoulder_angle_to_pwm( shoulder_angle_deg );
 
     gripper_servo.write(gripper_pwm);
     wrist_servo.write(wrist_pwm);
